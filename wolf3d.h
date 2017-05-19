d3b38699dcbcbcd3846398cfc15a9003879a6336
vhorbach <vhorbach@student.unit.ua>
2017-05-19 15:56:03 +0300

    19.05.2017 - 16:00

---
+++ wolf3d.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wolf3d.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vhorbach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/19 15:47:30 by vhorbach          #+#    #+#             */
+/*   Updated: 2017/05/19 15:47:34 by vhorbach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WOLF3D_h
+# define WOLF3D_H
+# define WIN_WID 1200
+# define WIN_HIGH 900
+# define IMG_WID 1200
+# define IMG_HIGH 900
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <math.h>
+# include "libft/libft.h"
+# include <mlx.h>
+# include <stdio.h>
+# include <pthread.h>
